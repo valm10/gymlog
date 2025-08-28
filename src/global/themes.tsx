2e774ccad67c33fb2be12d51266b1adbf067a57e
Vitor Lopes <vitorlopesmed@gmail.com>
2025-08-28 20:03:45 +0100

    themes added to project

--- src/global/themes.tsx
+++ src/global/themes.tsx
@@ -1,5 +1,12 @@
-import { Colors } from "chart.js";
-
 export const themas = {
-    Colors
-}
+    colors: {
+      primary: '#FF6A00',     // Main vibrant orange
+      orangeLight: '#FF944D', // Lighter orange (for hover or soft backgrounds)
+      orangeDark: '#CC5500',  // Darker orange (for pressed buttons or emphasis)
+      secondary: '#ffffff',   // White
+      lightGray: '#d7d8d7',   // Light gray
+      gray: 'gray',           // Standard gray
+      bgScreen: '#f1f7fa'     // Background color
+    }
+  }
+  
